08cdd1cbfd201ff69b6ef6656571e3d2ea557b61
Anik khan <anik9280@gmail.com>
2015-12-22 20:00:01 +0600

    sprout: update fingerprint from 6.0.1 release
    Change-Id: I2b953014719bffc04c998700e9c41b4ae0d9417d

--- tesla.mk
+++ tesla.mk
@@ -3,10 +3,10 @@ $(call inherit-product, device/google/sprout4/sprout4.mk)
 # Common Tesla stuff
 $(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0.1/MMB29K/2419427:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0.1 MMB29K 2419427 release-keys"
 
 PRODUCT_NAME := tesla_sprout4
-PRODUCT_DEVICE :=sprout4
+PRODUCT_DEVICE := sprout4
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := A1
